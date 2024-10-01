6369867801b4841d10d0127604ad96f7bbfd3ddf
JunlinHan <764545457@qq.com>
2024-10-01 11:20:25 +0200

    up d
    up d

--- projects/flex3d/js/splats.js
+++ projects/flex3d/js/splats.js
@@ -105,8 +105,8 @@ export async function setSplatScene(name, view) {
     view.progressDialog.close();
     // Not sure if these are meant to be public, but it's javascript anything goes :)
     var controls = new OrbitControls(camera, view.canvas, /*alpha=*/0.0, /*beta=*/0.0, /*radius=*/startRadius, /*enableKeyboardControls=*/false);
-    controls.minAngle = -50
-    controls.maxAngle = 50
+    controls.minAngle = -45
+    controls.maxAngle = 45
     controls.minZoom = 0.6
     controls.maxZoom = 0.9
     controls.zoomSpeed = 0.03
